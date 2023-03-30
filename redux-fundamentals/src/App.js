d21d6d34b5d48964b57952792ed86cf49719ec1b
ISMAIL <ismailjosim99@gmail.com>
2023-03-31 04:00:13 +0600

    Apply Redux With Raw JS

--- redux-fundamentals/src/App.js
+++ redux-fundamentals/src/App.js
@@ -1,7 +1,7 @@
 import Counter from './components/Counter';
 
+const App = () => {
 
-export default function App() {
     return (
         <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
             <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
@@ -12,4 +12,7 @@ export default function App() {
             </div>
         </div>
     );
-}
+
+};
+
+export default App;
