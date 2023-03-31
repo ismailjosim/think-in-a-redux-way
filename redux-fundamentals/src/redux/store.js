558a955872de20c6f574e1e84fa20dc7cd57aa82
ISMAIL <ismailjosim99@gmail.com>
2023-04-01 01:28:30 +0600

    2.10 Redux devTools

--- redux-fundamentals/src/redux/store.js
+++ redux-fundamentals/src/redux/store.js
@@ -1,42 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer';
 
-// create our first middleware
-
-const myLogger = store => next => action => {
-    console.log(`Action: ${ JSON.stringify(action) }`);
-    console.log(`Current State: ${ JSON.stringify(store.getState()) }`);
-
-
-    const upcomingState = [action].reduce(rootReducer, store.getState())
-
-    console.log(`Upcoming State: ${ JSON.stringify(upcomingState) }`);
-
-
-    // pass action
-    return next(action)
-}
+// all middleware
+import myLogger from './middlewares/myLogger'
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 
 
 
-
-const store = createStore(rootReducer, applyMiddleware(myLogger))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, myLogger)))
 
 export default store;
 
 
-
-
-
-
-/**
-//? curried function example
-const curried = (a) => (b) => (c) => {
-    return a * b * c
-}
-
-console.log(curried(2)(3)(5));
-
-
- */
