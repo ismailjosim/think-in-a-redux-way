80ab18206678a3b65238e515d81b70b655417e09
ISMAIL <ismailjosim99@gmail.com>
2023-03-31 16:47:50 +0600

    install redux & add counter actionType and actions

---
+++ redux-fundamentals/src/redux/counter/actionTypes.js
@@ -0,0 +1,2 @@
+export const INCREMENT = 'counter/increment'; // use this method for better understanding
+export const DECREMENT = 'counter/decrement';
---
+++ redux-fundamentals/src/redux/counter/actions.js
@@ -0,0 +1,18 @@
+import { DECREMENT, INCREMENT } from './actionTypes'
+
+
+// increment action
+export const increment = (value) => {
+    return {
+        type: INCREMENT,
+        payload: value
+    }
+}
+
+// decrement action
+export const decrement = (value) => {
+    return {
+        type: DECREMENT,
+        payload: value
+    }
+}
