a0f15b88f1fc754a4e05cb6e922b84132440e510
ismailjosim <ismailjosim99@gmail.com>
2024-02-27 17:13:56 +0600

    user register features added

--- Chat-Application/src/components/ui/Error.js
+++ Chat-Application/src/components/ui/Error.js
@@ -2,7 +2,7 @@ export default function Error({ message }) {
     return (
         <div className="flex items-center">
             <div className="relative bg-red-200 max-w-xl px-4 py-2 text-red-800 rounded shadow w-full">
-                <span className="block text-sm">{message}</span>
+                <span className="block text-sm">{ message }</span>
             </div>
         </div>
     );
