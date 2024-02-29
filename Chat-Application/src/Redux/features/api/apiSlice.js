5bba09306e890631fcb2231d5eb5d4154ecaa021
ismailjosim <ismailjosim99@gmail.com>
2024-02-29 11:46:17 +0600

    login functionality and send token for each api request

--- Chat-Application/src/Redux/features/api/apiSlice.js
+++ Chat-Application/src/Redux/features/api/apiSlice.js
@@ -3,7 +3,20 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: process.env.REACT_APP_API_URL
+        baseUrl: process.env.REACT_APP_API_URL,
+
+        // send bearer token for each api request
+        prepareHeaders: async (headers, { getState, endpoint }) => {
+            // get token from getState
+            const token = getState?.auth?.accessToken;
+            if (token) {
+                headers.set("authorization", `Bearer ${ token }`)
+            }
+
+            return headers;
+
+
+        }
     }),
     tagTypes: [],
     endpoints: (builder) => ({}),
--- Chat-Application/src/pages/Login.js
+++ Chat-Application/src/pages/Login.js
@@ -1,8 +1,46 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import logoImage from '../assets/images/lws-logo-light.svg'
 import Error from '../components/ui/Error'
+import { useEffect, useState } from 'react'
+import { useLoginMutation } from '../redux/features/auth/authApi'
 
 export default function Login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+
+    const [login, { data, isError, isLoading, error: showError }] = useLoginMutation()
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+
+        if (showError?.data) {
+            setError(showError?.data)
+        }
+
+        if (data?.accessToken && data?.user) {
+            navigate("/inbox")
+        }
+
+
+    }, [data, showError, navigate])
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setError("")
+        if (email !== "" && password !== "") {
+            login({
+                email,
+                password
+            })
+
+        } else {
+            setError("Your login is not correct")
+        }
+
+    }
 
     return (
         <div className='grid place-items-center h-screen bg-[#F9FAFB'>
@@ -20,7 +58,7 @@ export default function Login() {
                             Sign in to your account
                         </h2>
                     </div>
-                    <form className='mt-8 space-y-6' action='#' method='POST'>
+                    <form className='mt-8 space-y-6' onSubmit={ handleSubmit }>
                         <input type='hidden' name='remember' value='true' />
                         <div className='rounded-md shadow-sm -space-y-px'>
                             <div>
@@ -35,6 +73,8 @@ export default function Login() {
                                     required
                                     className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm'
                                     placeholder='Email address'
+                                    value={ email }
+                                    onChange={ (e) => setEmail(e.target.value) }
                                 />
                             </div>
                             <div>
@@ -49,6 +89,8 @@ export default function Login() {
                                     required
                                     className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm'
                                     placeholder='Password'
+                                    value={ password }
+                                    onChange={ (e) => setPassword(e.target.value) }
                                 />
                             </div>
                         </div>
@@ -68,12 +110,13 @@ export default function Login() {
                             <button
                                 type='submit'
                                 className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500'
+                                disabled={ isLoading }
                             >
                                 Sign in
                             </button>
                         </div>
 
-                        <Error message='There was an error' />
+                        { error && <Error message={ error } /> }
                     </form>
                 </div>
             </div>
